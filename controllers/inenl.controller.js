013cd34c4fc2a9c78fcce47aedf0ad587fa6aeca
Jonatan <aguilarupch@hotmail.com>
2022-01-22 14:42:14 -0600

    Segundo cambio al servicio para municipios

--- controllers/inenl.controller.js
+++ controllers/inenl.controller.js
@@ -1,6 +1,7 @@
 const db = require("../models");
 const inenl = db.inenl;
 const municipiosNuevoLeon = db.municipiosNuevoLeon;
+const municipiosChiapas= db.municipiosChiapas;
 const Op = db.Sequelize.Op;
 
 
@@ -10,13 +11,13 @@ exports.getDireccionNuevoLeon = (req, res) => {
     attributes: ['calle', 'int', 'ext', 'cp', 'e', 'm'],
     where: { curp: req.params.curp }
   }).then(data => {
-    municipiosNuevoLeon.findOne({
+    municipiosChiapas.findOne({
       attributes: ['nombreEntidad', 'nombreMunicipio'],
       where: { entidad: data.e, municipio: data.m }
     }).then(data2 => {
       data.e = data2.nombreEntidad;
       data.m = data2.nombreMunicipio;
-      res.send(data)
+      res.send(data);
     }).catch(err => {
       res.status(500).send({
         message: err    });
